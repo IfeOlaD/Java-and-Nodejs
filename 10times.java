7fecf4340938c85fa4722893db7ce49a269b4c73
Ifeoluwa <hermilovemi@gmail.com>
2025-05-12 00:24:17 +0100

    Lint

---
+++ 10times.java
@@ -0,0 +1,17 @@
+/**
+ * Entry point for the program
+ * A program that prints a String 10 times
+ * @param args, command line arguments
+ */
+
+public class Main{
+    public static void main(String[] args){
+	String roar = "Roar";
+	int i = 0;
+
+	while(i < 10){
+	    i++;
+	    System.out.println(roar);
+	}
+    }
+}
---
+++ rethod.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+	//rethod(5, 9);
+	System.out.println(rethod(5, 9));
+    }
+
+    public static int rethod(int x, int y){
+    	return x + y;
+    }
+}
